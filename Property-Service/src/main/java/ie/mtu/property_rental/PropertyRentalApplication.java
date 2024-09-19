1b1794ccb006d24e5bf8a093af59d8ba0c4158da
adam-omah <34926918+adam-omah@users.noreply.github.com>
2024-09-19 12:58:06 +0100

    Inital Micro Services Project with Rental and Property Services added.
    Inital Micro Services Project with Rental and Property Services added.

---
+++ Property-Service/src/main/java/ie/mtu/property_rental/PropertyRentalApplication.java
@@ -0,0 +1,13 @@
+package ie.mtu.property_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyRentalApplication.class, args);
+	}
+
+}
---
+++ Property-Service/src/test/java/ie/mtu/property_rental/PropertyRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.mtu.property_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
