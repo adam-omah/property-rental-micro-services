42e97b903ca3786d2a81a4e38a9f67c464313eb0
adam-omah <34926918+adam-omah@users.noreply.github.com>
2024-09-23 11:13:41 +0100

    Docker File and testing

---
+++ TestService/src/main/java/ie/mtu/property_rental_system/PropertyRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package ie.mtu.property_rental_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyRentalSystemApplication.class, args);
+	}
+
+}
---
+++ TestService/src/test/java/ie/mtu/property_rental_system/PropertyRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.mtu.property_rental_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
